97a278c23c4a0e882fe4e3ecd245b438dc9ed3c5
Tanvir Alam Sakib <1910006@student.ruet.ac.bd>
2026-04-07 23:17:15 +0600

    Fixed

--- backend/models/product.js
+++ backend/models/product.js
@@ -16,7 +16,7 @@ const productSchema = new mongoose.Schema({
   },
   category: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
+    ref: "category",
     required: [true, "Category is required"],
   },
   stock: {
